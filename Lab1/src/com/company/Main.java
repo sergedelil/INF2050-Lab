4a9cde9f51a87130fa1f176e25459a32fa1c8ad5
sergedelil <dogny.gnagnely_serge@courrier.uqam.ca>
2019-01-25 18:46:17 -0500

    question permutation lab1

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -6,6 +6,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        // question 2
+        int a = 5;
+        int b = 10;
+        System.out.println("Avant la permutation : a = "+a+" b = "+b);
+
+        int c;
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("Après la permutation : a = "+a+" b = "+b);
+
+        // question 3 4 5
+
         if (args.length != 1){
             System.out.println("Invalid argument...");
         }else {
